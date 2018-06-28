ed9dd416cd34632741b4e3ee1c8248e076e77da7
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-06-28 18:37:38 +0530

    save work

--- copy.sh
+++ copy.sh
@@ -1,1 +1,4 @@
-scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
+# Pig and choose
+
+#scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
+#scp -r doc/personal/resume.pdf akalia@linux.gp.cs.cmu.edu:www/doc/personal/resume.pdf
