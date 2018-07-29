d9a9a3924d9f97023b3cf4b11d94d63d333f2ddd
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-07-28 22:11:14 -0400

    updare

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 # Pig and choose
 
-#scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
+scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
 #scp -r doc/personal/resume.pdf akalia@linux.gp.cs.cmu.edu:www/doc/personal/resume.pdf
