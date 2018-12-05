3c688b2b56178072dd7e23ae5110d4f41e5c0a42
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-12-04 19:25:37 -0500

    save

--- copy.sh
+++ copy.sh
@@ -1,4 +1,7 @@
 # Pig and choose
 
 scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
+#scp -r *.css akalia@linux.gp.cs.cmu.edu:www/
 #scp -r doc/personal/resume.pdf akalia@linux.gp.cs.cmu.edu:www/doc/personal/resume.pdf
+
+#scp -r files/akalia_2018.jpg akalia@linux.gp.cs.cmu.edu:www/files/akalia_2018.jpg
