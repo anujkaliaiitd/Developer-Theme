cdd894d77ad93e8cdbbae8c76da62c009deea1cd
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-12-11 15:53:12 -0500

    materials

---
+++ doc/materials/update.sh
@@ -0,0 +1,9 @@
+cp ../research_statement/anuj_kalia_research.pdf .
+cp ../teaching_statement/anuj_kalia_teaching.pdf .
+cp ../diversity_statement/anuj_kalia_diversity.pdf .
+cp ../cover_letter/anuj_kalia_cover_letter.pdf .
+cp ~/resume/anuj_kalia_resume.pdf .
+
+#wget http://www.cs.cmu.edu/~akalia/doc/sigcomm14/herd_readable.pdf
+#wget http://www.cs.cmu.edu/~akalia/doc/nsdi15/gopt_nsdi.pdf
+#wget http://www.cs.cmu.edu/~akalia/doc/nsdi19/erpc_nsdi19.pdf
