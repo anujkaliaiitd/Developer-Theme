a0f670fcc7d71be0509eff389aaa5d81bdb543da
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-06-07 11:05:16 -0400

    save

--- copy.sh
+++ copy.sh
@@ -1,8 +1,8 @@
 # Pig and choose
 
-#scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
+scp -r *.html akalia@linux.gp.cs.cmu.edu:www/
 #scp -r doc/nsdi19/erpc_nsdi_talk.pdf akalia@linux.gp.cs.cmu.edu:www/doc/nsdi19/erpc_nsdi_talk.pdf
-scp -r doc/materials/* akalia@linux.gp.cs.cmu.edu:www/doc/materials/
+#scp -r doc/materials/* akalia@linux.gp.cs.cmu.edu:www/doc/materials/
 #scp -r *.css akalia@linux.gp.cs.cmu.edu:www/
 #scp -r doc/personal/resume.pdf akalia@linux.gp.cs.cmu.edu:www/doc/personal/resume.pdf
 
